aa2cec70696bd5dd9dd9a8c0653f87977bea1409
HoustonWeHaveABug <HoustonWeHaveABug@users.noreply.github.com>
2024-12-30 14:24:43 +0100

    Update mondrian_rectangles.sh

--- mondrian_rectangles.sh
+++ mondrian_rectangles.sh
@@ -1,16 +1,20 @@
 #!/bin/bash
-if [ $# -ne 6 ]
+if [ $# -ne 7 ]
 then
-	echo "Usage: $0 width rotate_flag defect_a defect_b options_min verbose_flag"
+	echo "Usage: $0 width_inf width_sup rotate_flag defect_a defect_b options_min verbose_flag"
 	exit 1
 fi
 make -f mondrian.make
-let HEIGHT=1
 let WIDTH=$1
-while [ $HEIGHT -le $WIDTH ]
+while [ $WIDTH -le $2 ]
 do
-	echo "Rectangle ${HEIGHT}x${WIDTH}"
-	echo $HEIGHT $WIDTH $2 $3 $4 $5 $6 | ./mondrian
-	let HEIGHT=$HEIGHT+1
+	let HEIGHT=1
+	while [ $HEIGHT -le $WIDTH ]
+	do
+		echo "Rectangle ${HEIGHT}x${WIDTH}"
+		echo $HEIGHT $WIDTH $3 $4 $5 $6 $7 | ./mondrian
+		let HEIGHT=$HEIGHT+1
+	done
+	let WIDTH=$WIDTH+1
 done
 exit 0
