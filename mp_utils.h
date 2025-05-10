93f615e7f6be05a3319ec62cda890d9c8b65f1f0
HoustonWeHaveABug <HoustonWeHaveABug@users.noreply.github.com>
2025-05-10 16:03:54 +0200

    updated mp_utils

--- mp_utils.h
+++ mp_utils.h
@@ -5,7 +5,14 @@ typedef struct {
 mp_t;
 
 int mp_new(mp_t *);
-void mp_print(const char *, const mp_t *);
+int mp_from_val(mp_t *, unsigned long);
 int mp_inc(mp_t *);
+int mp_add(mp_t *, mp_t *);
+int mp_sub(mp_t *, mp_t *);
+int mp_mul(mp_t *, mp_t *);
+int mp_divmod(mp_t *, mp_t *, mp_t *);
+int mp_copy(mp_t *, mp_t *);
+int mp_compare(const mp_t *, const mp_t *);
 int mp_eq_val(const mp_t *, unsigned long);
+void mp_print(const char *, const mp_t *);
 void mp_free(mp_t *);
